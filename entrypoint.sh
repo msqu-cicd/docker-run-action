9aca642bc9a3ac2488b889fb1f1e4872f931a24d
Foat Akhmadeev <foat@fmga.ru>
2024-12-16 16:55:48 +0300

    cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,4 +28,4 @@ fi
 
 echo "Docker run options: ${INPUT_OPTIONS}"
 
-exec docker run --rm -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS --entrypoint="$INPUT_SHELL" "$INPUT_IMAGE" -c "${INPUT_RUN//$'\n'/;}"
+docker run --rm -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS --entrypoint="$INPUT_SHELL" "$INPUT_IMAGE" -c "${INPUT_RUN//$'\n'/;}"
